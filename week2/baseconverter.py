8ae19db4e03379633ee4440e583eab2f9c314351
Alexander Mitchell <amitchellofficial@gmail.com>
2019-08-22 00:55:23 -0400

    adds sys args

--- week2/baseconverter.py
+++ week2/baseconverter.py
@@ -1,1 +1,12 @@
 # This program converts a given decimal number into base 10
+# I also updated to Python 3 Haha
+import sys
+
+
+# I input a number and the base thar I want to convert it to
+btennum = sys.argv[1]
+resultbase = sys.argv[2]
+
+# Test for the sys args. I'll work on this tomorrowit's 12:54 in the morning and I have the coding bug. 
+# I need sleep.
+print(str(btennum)+" and "+str(resultbase))
