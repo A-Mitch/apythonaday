ea0357a902f85f4b545a113f481ab3989b284e1e
Alexander Mitchell <amitchellofficial@gmail.com>
2019-08-31 16:47:16 -0400

    adds some code to drone simulator

---
+++ week3/dronesimulator.py
@@ -0,0 +1,8 @@
+# This program siulates flying drones doe my friend Juan who is too scared to fly his
+# This program was created by Alexander Mitchell
+# So this is using python 2 even thoug
+
+# Start up the simulator
+startup = input("Welcome to Drone Flight Navigator 3005! \n Did you want to start to flying? (Yes/No)  ")
+print(startup)
+
